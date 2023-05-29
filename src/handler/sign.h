6bd49f7f689cbfe4fbf022c7bb86fdf682f3a04c
Alexandre Paillier <alexandre.paillier@ledger.fr>
2023-05-29 10:23:20 +0200

    Sign operation is now interactive

--- src/handler/sign.h
+++ src/handler/sign.h
@@ -1,10 +1,11 @@
 #ifndef SIGN_H_
 #define SIGN_H_
 
-#include <stdint.h>
+#include <stdbool.h>
 
 #define SIGNING_ROOT_SIZE 32
 
 int handler_sign(uint32_t index, uint8_t *signing_root);
+void sign(bool approved);
 
 #endif  // SIGN_H_
--- src/ui/display.h
+++ src/ui/display.h
@@ -1,1 +1,6 @@
 #pragma once
+
+extern char g_index_str[10 + 1];
+extern char g_signing_root_str[2 + (SIGNING_ROOT_SIZE * 2) + 1];
+
+void ui_display_sign(void);
