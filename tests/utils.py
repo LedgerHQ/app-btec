2854c1a748c66fda1a4fc634236d49de900c08a3
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-03-23 17:22:57 +0100

    Replaced pysh3 with pycryptodome in Ragger tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from hashlib import sha256
-from sha3 import keccak_256
+import functools
 
 from ecdsa.curves import SECP256k1
 from ecdsa.keys import VerifyingKey
 from ecdsa.util import sigdecode_der
+from Crypto.Hash import keccak
 
 
 ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
@@ -19,5 +20,5 @@ def check_signature_validity(public_key: bytes, signature: bytes, message: bytes
     )
     return pk.verify(signature=signature,
                      data=message,
-                     hashfunc=keccak_256,
+                     hashfunc=functools.partial(keccak.new, digest_bits=256),
                      sigdecode=sigdecode_der)
